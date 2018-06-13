852ed5f69a60afe93f3ee945b415d0263c3c0dae
ryandecoster <36908739+ryandecoster@users.noreply.github.com>
2018-06-13 12:23:45 -0500

    finished django app

---
+++ Django_App/main/apps/blogs_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+def index(request):
+    response = 'Placeholder to later display all the list of blogs.'
+    return HttpResponse(response)
+
+def new(request):
+    response = 'Placeholder to display a new form to create a new blog.'
+    return HttpResponse(response)
+
+def create(request):
+    return redirect('/')
+
+def show(request, number):
+    return HttpResponse("Placeholder to display blog " + number + ".")
+
+def edit(request, number):
+    return HttpResponse("Placeholder to edit blog " + number + ".")
+
+def destroy(request, number):
+    return redirect('/')
