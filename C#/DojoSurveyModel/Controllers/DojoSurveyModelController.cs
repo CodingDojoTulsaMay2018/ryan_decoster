559a2a32537c60e066d62220f74effe36df2f305
Ryan DeCoster <ryandecoster26@yahoo.com>
2018-07-06 14:21:47 -0500

    finished dojosurvey with models

---
+++ C#/DojoSurveyModel/Controllers/DojoSurveyModelController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dojosurveymodel.Models;
+
+namespace dojosurveymodel.Controllers
+{
+    public class DojoSurveyModelController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("survey")]
+        public IActionResult Survey(string name, string location, string language, string comment) {
+            Ninja user = new Ninja() {
+                Name = name,
+                Location = location,
+                Language = language,
+                Comment = comment
+            };
+            return View(user);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ C#/DojoSurveyModel/Models/Ninja.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace dojosurveymodel.Models {
+    public class Ninja {
+        public string Name {get;set;}
+        public string Location {get;set;}
+        public string Language {get;set;}
+        public string Comment {get;set;}
+    }
+}
