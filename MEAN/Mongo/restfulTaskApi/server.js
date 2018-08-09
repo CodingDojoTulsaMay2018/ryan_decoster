39faa969d1f50255a1050b4a4fae78ce7decf643
Ryan DeCoster <ryandecoster26@yahoo.com>
2018-08-09 13:25:00 -0500

    initial commit

---
+++ MEAN/Mongo/restfulTaskApi/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8000;
+   
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './static')));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs'); 
+
+require("./server/config/mongoose");
+require("./server/config/routes")(app);
+
+app.listen(port, () => {
+    console.log(`all systems go on port ${port}!`);
+});
